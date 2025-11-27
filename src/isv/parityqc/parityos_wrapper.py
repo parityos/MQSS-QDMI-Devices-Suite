519fcf5cbcbe91e5fcedc51d57cdca8077ac2f20
Reinhard Stahn <r.stahn@parityqc.com>
2025-11-27 10:29:07 +0100

    Refactor wrapper script

--- src/isv/parityqc/parityos_wrapper.py
+++ src/isv/parityqc/parityos_wrapper.py
@@ -6,9 +6,15 @@ Auxiliary script to conveniently use parityos functionality from parityos.cpp vi
 # TODO: Make sure parityos is installed whenever this script is executed.
 
 from functools import wraps
-from typing import Optional
 
+# TODO: it would be better to explicitly signal an error. E.g. by sending back some special object
+# which possibly contains an error message.
 def exceptions_to_none(func):
+    """The functions we call from C code must not raise any exceptions. Otherwise the whole process
+    just terminates. We have to avoid this, even at the cost of worse error reporting.
+
+    This is also the reason why we import all but the builtin packages within the function body.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
@@ -20,11 +26,11 @@ def exceptions_to_none(func):
 
 
 # TODO: this is just toy code for the fake_backend. The actual parityos backend does not need this.
-submission_results: dict[int, str] = dict()
+_submission_results: dict[int, str] = dict()
 
 
 @exceptions_to_none
-def create_parityos_client(base_url: str) -> Optional["HTTPClient"]:
+def create_parityos_client(base_url: str) -> "HTTPClient":
     """Create the parityos http client.
 
     If the creation succeeds this implies that authentication worked.
@@ -42,7 +48,7 @@ def create_parityos_client(base_url: str) -> Optional["HTTPClient"]:
 
 # TODO: this is still a fake implementation. The real one would use the client.
 @exceptions_to_none
-def submit_job(client: "HTTPClient", program: str) -> int | None:
+def submit_job(client: "HTTPClient", program: str) -> int :
     """Submit a program to the parityos web service to be compiled to a circuit.
 
     It returns the submission id which can be used to identify the job results once they are
@@ -62,16 +68,20 @@ def submit_job(client: "HTTPClient", program: str) -> int | None:
     return submission_id
 
 
-# TODO: this is still a fake implementation.
 @exceptions_to_none
 def poll_result(submission_id: int) -> str | None:
     """Return the compilation result of a job or `None` if not available.
 
-    Right now it also returns `None` if anything goes wrong.
+    NOTE: If the result is available it is removed from the global dict holding the results. This
+    implies that if the result is never polled a memory leak can occur.
+
+    NOTE: Right now it also returns `None` if anything goes wrong.
     """
-    global submission_results
-    if submission_id in submission_results:
-        return submission_results[submission_id]
+    global _submission_results
+    if submission_id in _submission_results:
+        result = _submission_results[submission_id]
+        del _submission_results[submission_id]
+        return result
     else:
         return None
 
@@ -94,7 +104,7 @@ def _next_submission_id():
 
 
 def _fake_backend(content: str, submission_id) -> None:
-    global submission_results
+    global _submission_results
     result = None
 
     if content == "hello":
@@ -104,4 +114,4 @@ def _fake_backend(content: str, submission_id) -> None:
     else:
         result = "invalid input"
 
-    submission_results[submission_id] = result
+    _submission_results[submission_id] = result
